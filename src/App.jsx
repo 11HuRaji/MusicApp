d5dbd520bc919abd2bb28779e5f174fa5e6c6b4c
Hussein Raji <111huraji@gmail.com>
2023-07-10 16:47:44 +0100

    made music app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import LikeButton from "./LikeButton";
+import jbImage from "./assets/images/jb_image.jpg";
+import jbImage2 from "./assets/images/jb_grammys.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tracks, setTracks] = useState([]);
+  console.log(tracks);
+  const [inputText, setInputText] = useState("");
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Justin Beiber's Tracklist</h1>
+        <img src={jbImage} alt="An image of Justin Beiber" />
+        <img src={jbImage2} alt="Another image of Justin Beiber" />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Bio: He is the son of Pattie Mallette and Jeremy Bieber. He is of
+          French-Canadian, Irish, German, English, and Scottish descent. He has
+          three younger half-siblings via his father. Growing up, he showed a
+          strong interest in music and taught himself to play many instruments,
+          including, guitar, drums, piano and trumpet.
         </p>
+        <ul id="tracklist">
+          <li>
+            Baby 3:33 <LikeButton />
+          </li>
+          <li>
+            One Time 3:35 <LikeButton />
+          </li>
+          <li>
+            Never Say Never 3:47 <LikeButton />
+          </li>
+          <li>
+            Somebody to Love Remix 3:40 <LikeButton />
+          </li>
+          {tracks}
+        </ul>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setTracks((state) => [
+              ...state,
+              <li>
+                {inputText} <LikeButton />
+              </li>,
+            ]);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Enter your favourite track"
+          />
+          <button type="submit">Add Track</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
